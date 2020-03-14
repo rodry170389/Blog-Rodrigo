069e00ac3dea48bf5e455a933af73c969017dd9c
Rodrigo <rodry170389@gmail.com>
2020-03-14 01:30:43 +0100

    Pagina con 4 imagenen version casi final

--- js/paginaprincipal.js
+++ js/paginaprincipal.js
@@ -9,7 +9,7 @@ function datospantalla(){
         document.getElementById("linkcontacto").href = "#marcaprincipal";
 
         document.getElementById("linkasideuniversidad").href = "#marcaprincipal";
-        document.getElementById("linkasideproyecto").href = "#marcaprincipal";
+        document.getElementById("linkabajoproyectos").href = "#marcaprincipal";
         document.getElementById("linkasidecontacto").href = "#marcaprincipal";
     }else{
         document.getElementById("linkuniversidad").href = "#marcafinal";
@@ -17,7 +17,7 @@ function datospantalla(){
         document.getElementById("linkcontacto").href = "#marcafinal";
 
         document.getElementById("linkasideuniversidad").href = "#marcafinal";
-        document.getElementById("linkasideproyecto").href = "#marcafinal";
+        document.getElementById("linkabajoproyectos").href = "#marcafinal";
         document.getElementById("linkasidecontacto").href = "#marcafinal";
     }
 }
@@ -30,7 +30,7 @@ window.addEventListener("resize", function(){
         document.getElementById("linkcontacto").href = "#marcaprincipal"; 
 
         document.getElementById("linkasideuniversidad").href = "#marcaprincipal";
-        document.getElementById("linkasideproyecto").href = "#marcaprincipal";
+        document.getElementById("linkabajoproyectos").href = "#marcaprincipal";
         document.getElementById("linkasidecontacto").href = "#marcaprincipal";
     }else{
         document.getElementById("linkuniversidad").href = "#marcafinal";
@@ -38,7 +38,7 @@ window.addEventListener("resize", function(){
         document.getElementById("linkcontacto").href = "#marcafinal";
 
         document.getElementById("linkasideuniversidad").href = "#marcafinal";
-        document.getElementById("linkasideproyecto").href = "#marcafinal";
+        document.getElementById("linkabajoproyectos").href = "#marcafinal";
         document.getElementById("linkasidecontacto").href = "#marcafinal";
     }
   });
@@ -70,4 +70,67 @@ function mostrarDocumentos(){
     document.getElementById('proyectos').style.display = 'none'
     document.getElementById('documentos').style.display = 'block'
     
+}
+/*JavaScropit para imagenes Proyecto y documentación*/
+
+function muestraimagen(){
+    document.getElementById('myModal').style.display = "block";
+    document.getElementById("img01").src = document.getElementById('myImg').src;
+    document.getElementById("img01").alt = document.getElementById('myImg').alt;
+    document.getElementById("caption").innerHTML = document.getElementById('myImg').alt;
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+function cerrarimagen() { 
+    document.getElementById('myModal').style.display = "none";
+}
+
+/*JavaScropit para imagenes Proyecto y documentación*/
+
+function muestraimagen2(){
+    document.getElementById('myModal2').style.display = "block";
+    document.getElementById("img02").src = document.getElementById('myImg2').src;
+    document.getElementById("img02").alt = document.getElementById('myImg2').alt;
+    document.getElementById("caption2").innerHTML = document.getElementById('myImg2').alt;
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close2")[0];
+
+// When the user clicks on <span> (x), close the modal
+function cerrarimagen3() { 
+    document.getElementById('myModal3').style.display = "none";
+}
+
+function muestraimagen3(){
+    document.getElementById('myModal3').style.display = "block";
+    document.getElementById("img03").src = document.getElementById('myImg3').src;
+    document.getElementById("img03").alt = document.getElementById('myImg3').alt;
+    document.getElementById("caption3").innerHTML = document.getElementById('myImg3').alt;
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close3")[0];
+
+// When the user clicks on <span> (x), close the modal
+function cerrarimagen2() { 
+    document.getElementById('myModal3').style.display = "none";
+}
+
+function muestraimagen4(){
+    document.getElementById('myModal4').style.display = "block";
+    document.getElementById("img04").src = document.getElementById('myImg4').src;
+    document.getElementById("img04").alt = document.getElementById('myImg4').alt;
+    document.getElementById("caption4").innerHTML = document.getElementById('myImg4').alt;
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close4")[0];
+
+// When the user clicks on <span> (x), close the modal
+function cerrarimagen4() { 
+    document.getElementById('myModal4').style.display = "none";
 }
